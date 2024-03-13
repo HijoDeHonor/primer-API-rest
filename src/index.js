364b2d7b245b73fea6250447b4424d92f18d135f
Nicolas Grandoli <41nicograndoli@gmail.com>
2024-03-13 16:38:09 -0300

    se hizo mas legible el codigo

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ const express =require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser')//esta
+
+
 //settings
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());//esta
 
-// Routes
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // Permitir acceso desde cualquier origen
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+// Routes
 app.use(require('./routes/index'));
 app.use('/api/movies',require('./routes/movies'));
 app.use('/api/users', require('./routes/users'));
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-
+const crypto = require('crypto')
 const _ = require('underscore')
 
 const movies = require('../sample.json');
@@ -11,33 +11,41 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const { title, director, year, rating, imgURL} = req.body;
-    if (title && director && year && rating) {
-        const id = movies.length + 1;
-        const nuevaPeli = { ...req.body, id };
-        movies.push(nuevaPeli);
-        res.json(movies);
-    } else {
-        res.status(500).json({ error: "Ocurrio un ERROR." });
+
+    if (!req.body.title || !req.body.director || !req.body.year || !req.body.rating || !req.body.imgURL) {//siempre tratar de ir por el camino mas corto y
+        res.status(400).json({ error: "Faltan datos." });                                                   //si es un error ponerlo al principio
+        return;                                                                                           //para al retornar no usar tantas lineas para arriba.
     }
+    const id = crypto.randomUUID(); //se usa esta linea para no repetir id, en vez de la de abajo que prodrian llegar a repetirse
+    //const id = movies[movies.length-1].id + 1; es la linea que se trata de evitar
+    const nuevaPeli = { ...req.body, id };
+    movies.push(nuevaPeli);
+    res.status(201).json(movies); // retornamos el status 201 (creado) y adicionalmente el arreglo nuevo.
 });
 
 
 router.delete('/:id', (req, res) => {
+    let isDelete = false;
     const { id } = req.params;
     _.each(movies, (movie, i) => {
         if (movie.id == id) {
             movies.splice(i, 1);
+            isDelete = true;
         }
     });
-    res.send(movies);
+
+    if (!isDelete) {
+        res.status(404).json({ error: "No se encontro la id" });
+        return;
+    }
+    res.json(movies);
 });
 
 
 router.put('/:id', (req, res) => {
     const { id } = req.params;
     const { title, director, year, rating, imgURL } = req.body;
-    if (title && director && year && rating && imgURL ) {
+    if (title && director && year && rating && imgURL) {
         _.each(movies, (movie, i) => {
             if (movie.id == id) {
                 movie.title = title;
